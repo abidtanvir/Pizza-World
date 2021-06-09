913e2ac09d5bd9135ae87d7fc8bf6296f47ed479
abidtanvir <tanvirabid.aiub@gmail.com>
2021-06-09 21:42:39 +0200

    Order added to cart

--- addToCart.php
+++ addToCart.php
@@ -4,3 +4,18 @@
    $dbname      = "dbname = postgres";
    $credentials = "user = postgres password=1234";
 
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   
+   if( isset($_GET['addtocart']) )
+	{
+		$itemid = $_GET['checkbbox'];
+		
+		foreach($itemid as $id )
+		{
+		
+		$sql = pg_query($db, "select  addtocart('$id');");
+		
+		}
+		
+	}
+	?>
