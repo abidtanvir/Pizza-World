f33a78a4e6189b16d031cc61eebe18aae06c6c3f
abidtanvir <tanvirabid.aiub@gmail.com>
2021-06-15 21:59:16 +0200

    Delete Ingredients

--- deleteIngredient.php
+++ deleteIngredient.php
@@ -6,5 +6,13 @@
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    
-   
+   if( isset($_GET['delIngId']) )
+	{
+		$ingID = $_GET['delIngId'];
+		
+		$sql = pg_query($db, "select  deleteingredient('$ingID');");
+		
+		
+		echo "<meta http-equiv='refresh' content='0;url=manageIngredients.php'>";
+	}
 	?>
