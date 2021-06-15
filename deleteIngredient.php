abd2e1848e5b281c311ea31e24bb8ef6a2cae075
abidtanvir <tanvirabid.aiub@gmail.com>
2021-06-15 21:58:41 +0200

    delete Ingredient

---
+++ deleteIngredient.php
@@ -0,0 +1,10 @@
+<?php
+   $host        = "host = 127.0.0.1";
+   $port        = "port = 5432";
+   $dbname      = "dbname = postgres";
+   $credentials = "user = postgres password=1234";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   
+   
+	?>
